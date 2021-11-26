95c9a900a418c6efb05c61d2011746682f806c4e
Deep <Deep@johann>
2021-11-26 02:58:58 +0100

    initial project file structure

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+
+`;
+
+export default function Home() {
+  return (
+    <Wrapper>
+      <div>Hello, World!</div>
+    </Wrapper>
+  );
+}
